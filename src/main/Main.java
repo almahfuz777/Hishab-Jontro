6f1dfb4d182790fbc5e3d7dd7cc9005ad94fb9eb
Abdullah Al Mahfuz <almahfuz777@gmail.com>
2024-01-19 21:16:41 +0600

    code updated

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,34 +1,7 @@
 package main;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-
-public class Main extends Application {
+public class Main {
     public static void main(String[] args) {
-        launch();
+        Run.main(args);
     }
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        //Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("Main.fxml"));
-        Scene scene = new Scene(loader.load());
-        //Scene scene = new Scene(root);
-        scene.setFill(Color.TRANSPARENT);
-
-        stage.setScene(scene);
-        stage.initStyle(StageStyle.TRANSPARENT);
-        stage.setResizable(false);
-        stage.setTitle("Hishab Jontro");
-        stage.getIcons().add(new Image("/images/icon.png"));
-        ((Controller)loader.getController()).init(stage);   //
-        stage.show();
-   }
-
 }
